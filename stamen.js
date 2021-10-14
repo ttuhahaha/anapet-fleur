58d7f7934f96e2f4d701e1b38674efafea9a77a3
ttuhahaha <91697242+ttuhahaha@users.noreply.github.com>
2021-10-13 23:33:00 -0400

    Add files via upload

--- stamen.js
+++ stamen.js
@@ -1,56 +1,51 @@
 function drawstamen() {
-  if (stamens.length == 0 && maxstamen > 0) {
-    stamens = Array(15)
+  if (particles.length == 0 && maxstamen > 0) {
+    particles = Array(15)
       .fill()
       .map((p) => new Stamen());
   }
-  for (let stamen of stamens) {
-    if (frameCount % 100 == stamen.offset) {
-      push();
-      stroke(stamen.color);
+  for (let s of particles) {
+    if (frameCount % 100 == s.offset) {
+      stroke(s.color);
       strokeWeight(by / 12);
-      for (let i = 0; i < LL.R / 2; i++) {
-        line(stamen.pos.x, stamen.pos.y, stamen.pos.x + nz(1) * LL.R / 7, stamen.pos.y + nz(1) * LL.R / 7)
-      }
-      pop();
+      for (let i = 0; i < LLR / 2; i++) 
+        line(s.pos.x, s.pos.y, s.pos.x + nz(1) * LLR / 7, s.pos.y + nz(1) * LLR / 7)     
       if (stamnum < maxstamen) {
-        stamens.push(new Stamen());
+        particles.push(new Stamen());
         stamnum++;
       }
     } else {
-      stamen.color = color(stamen.hu + stamen.LS, 100, 50, stamen.LS);
-      stamen.velocity.add(stamen.acceleration);
-      stamen.velocity.setMag(layers.length > 4 ? LL.R/50 : LL.R / 90);
-      stamen.pos.add(stamen.velocity);
-      stamen.LS += 0.002;
-      stroke(stamen.color);
+      s.color.setAlpha(s.LS);
+      s.vel.add(s.acc);
+      s.vel.setMag(layers.length > 4 ? LLR / 50 : LLR / 90);
+      s.pos.add(s.vel);
+      s.LS += 0.002;
+      stroke(s.color);
       strokeWeight(by / 5);
-      line(
-        stamen.prevpos.x,
-        stamen.prevpos.y,
-        stamen.pos.x,
-        stamen.pos.y
-      );
-      stamen.prevpos = stamen.pos.copy();
+      line(s.prevpos.x, s.prevpos.y, s.pos.x, s.pos.y);
+      s.prevpos = s.pos.copy();
     }
   }
-  stamens = stamens.filter((p) => frameCount % 100 != p.offset);
-  if (stamens.length == 0) return frameCount;
+  particles = particles.filter((p) => frameCount % 100 != p.offset);
+  if (particles.length == 0) {
+    maxstamen = 0;
+    startframe = frameCount;
+  }
 }
 
 class Stamen {
   constructor() {
-    this.pos = createVector(home.x - nz(1) * LL.R / 2, home.y - nz(1) * LL.R / 2);
-    this.acceleration = createVector(0, 0.001 * bx);
-    this.velocity = p5.Vector.sub(this.pos, home);
-    this.velocity.setMag(bx);
+    this.pos = createVector(home.x - nz(1) * LLR / 2, home.y - nz(1) * LLR / 2);
+    this.acc = createVector(0, 0.001 * bx);
+    this.vel = p5.Vector.sub(this.pos, home);
+    this.vel.setMag(bx);
     let h = home.copy();
-    h.setMag(bx*2);
-    this.velocity.add(h);
+    h.setMag(bx * 2);
+    this.vel.add(h);
     this.LS = -0.01;
     this.hu = hu + nz() * 160;
-    this.color = color(this.hu, 100, 50, 1);
-    this.offset = (frameCount + floor(map(nz(), 0.1, 1, 10, 99))) % 100; 
+    this.color = color(this.hu, 100, 50);
+    this.offset = (frameCount + floor(map(nz(), 0.1, 1, 10, 99))) % 100;
     if (this.offset == 0) this.offset = 50;
     this.prevpos = this.pos.copy();
   }
